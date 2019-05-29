403171211a3b8cac19b35748b28c0b4d3bec470b
jesse collier <jesse.collier97@gmail.com>
2019-05-29 19:17:20 +1200

    finished card seed data

--- seeds/cards.js
+++ seeds/cards.js
@@ -7,7 +7,38 @@ exports.seed = function(knex, Promise) {
       return knex('cards').insert([
         {
           id: 301, 
-          colName: 'rowValue1'
+          columnId: 201,
+          title: "seed data",
+          description: "inputing the seed data",
+          position: "(0,0)"
+        },
+        {
+          id: 302, 
+          columnId: 201,
+          title: "migrations",
+          description: "making migrations",
+          position: "(1,0)"
+        },
+        {
+          id: 303, 
+          columnId: 202,
+          title: "react",
+          description: "doing all the components",
+          position: "(0,0)"
+        },
+        {
+          id: 303, 
+          columnId: 202,
+          title: "redux",
+          description: "finished the store",
+          position: "(0,1)"
+        },
+        {
+          id: 303, 
+          columnId: 203,
+          title: "readme",
+          description: "user stories and stretch goals",
+          position: "(0,0)"
         },
       ]);
     });
