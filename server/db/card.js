3cf48e5b9ac8ad3bd98d75a3a1b38ddb19d8de30
jesse collier <jesse.collier97@gmail.com>
2019-06-04 22:20:56 +1200

    forgot one

--- server/db/card.js
+++ server/db/card.js
@@ -1,20 +1,24 @@
-const connection = require('./connection')
+const connection = require("./connection");
 
 function getCards(columnId, db = connection) {
-  return //gets all the cards for a specific column
+  return; //gets all the cards for a specific column
 }
 
 function deleteCard(cardId, db = connection) {
-  return //deletes a specific card
+  return; //deletes a specific card
 }
 function editCardName(card, db = connection) {
-  return
+  return;
 }
 function editCardDescription(card, db = connection) {
-  return
+  return;
 }
 function editCardPos(card, db = connection) {
-  return
+  return;
+}
+function moveCard(cardId, newColumn, db = connection) {
+  return; // moves card from one column to another
+  // dont know if im gonna move the position aswell
 }
 
 module.exports = {
@@ -22,5 +26,6 @@ module.exports = {
   deleteCard,
   editCardName,
   editCardDescription,
-  editCardPos
-}
+  editCardPos,
+  moveCard
+};
